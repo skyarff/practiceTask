f32bfddbd95133757d56ee2004daad6587776e0c
unknown <skyarff@gmail.com>
2024-06-24 12:53:44 +0400

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vueTry2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
